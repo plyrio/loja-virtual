a8acfc762e0a67bc496ef2328669fa1ec08a887d
Pedro Lyrio <138774252+plyrio@users.noreply.github.com>
2024-03-13 17:59:01 -0300

    Atualizar o vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/loja-virtual/',
-  build: {
-        outDir: 'doc',
-  }
-//  base: '/loja-virtual/',
-//teste 
+// base: '/loja-virtual/',
 })
