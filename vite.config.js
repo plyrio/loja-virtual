6f84ae7bf8ae39c4f7d4fff3b6a2f8271f801364
Pedro Lyrio <138774252+plyrio@users.noreply.github.com>
2024-03-13 15:54:51 +0000

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/loja-virtual/'
+  base: '/loja-virtual/',
+    build: {
+          outDir: 'doc',
+    }
 //  base: '/loja-virtual/', 
 })
